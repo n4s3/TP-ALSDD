ca974c0e8fc24aec418b3214b487bb35c6b8b66e
Anes <97311029+n4s3@users.noreply.github.com>
2024-03-24 17:50:43 +0100

    Create Funcs.h

---
+++ Funcs.h
@@ -0,0 +1,8 @@
+#ifndef FUNCS_H_INCLUDED
+#define FUNCS_H_INCLUDED
+#include "AbstractMachine.h"
+// transfer amount from bankaccount a to bankaccount b
+void TransferAmount(BankAccount *A, BankAccount *B, float Amount);
+int CheckAmount(BankAccount *A);
+void CheckHistory(BankAccount *A);
+#endif
