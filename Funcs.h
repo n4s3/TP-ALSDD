bb6957558888cb788deb1b8d748411bd088072c1
Anes <97311029+n4s3@users.noreply.github.com>
2024-03-29 18:00:35 +0100

    Update Funcs.h

--- Funcs.h
+++ Funcs.h
@@ -1,9 +1,10 @@
 #ifndef FUNCS_H_INCLUDED
 #define FUNCS_H_INCLUDED
 #include "AbstractMachine.h"
-// transfer amount from bankaccount a to bankaccount b
-void TransferAmount(BankAccount *A, BankAccount *B, float Amount);
-int CheckAmount(BankAccount *A);
-void CheckHistory(BankAccount *A, Date D);
+void TransferAmount(BankAccount *h , int N1,int N2, float Amount,Date D);
+int CheckAmount(BankAccount *h , int Number );
+void CheckHistory(BankAccount *h,int Number ,  Date D);
 void Deposit( BankAccount * h , int Number , float De , Date D ) ;
+char *Printdate(Transaction *A);
+void EditCodeAndOrCustumer( BankAccount * h , int Number );
 #endif
