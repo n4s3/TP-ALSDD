ba700d7289de402c8e0acf653f69c189ee264a55
Anes <mranesbelarbi@gmail.com>
2024-04-02 16:21:02 +0100

    added stuff

--- Read-Write-Functions.c
+++ Read-Write-Functions.c
@@ -1,12 +1,13 @@
+#include "Read-Write-Functions.h"
+#include "AbstractMachine.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "AbstractMachine.h"
-#include "Read-Write-Functions.h"
 #define max_len 256
 const char *file_name = "Accounts.txt";
 
-void addAccountToList(BankAccount **head, char FirstName[15], char LastName[15], int Number, int Code, float Balance){
+void addAccountToList(BankAccount **head, char FirstName[15], char LastName[15],
+                      int Number, int Code, float Balance) {
   BankAccount *new_account;
   createAccount(&new_account);
   strcpy(new_account->Name.First, FirstName);
@@ -14,34 +15,38 @@ void addAccountToList(BankAccount **head, char FirstName[15], char LastName[15],
   new_account->Number = Number;
   new_account->Code = Code;
   new_account->Balance = Balance;
-  if (*head == NULL){
+  if (*head == NULL) {
     *head = new_account;
   } else {
     BankAccount *current = *head;
-    while (current->nextBAcc != NULL){
+    while (current->nextBAcc != NULL) {
       current = next_Account(current);
     }
     current->nextBAcc = new_account;
     new_account->nextBAcc = NULL;
   }
 }
-void readAccountInfo(const char *file_name, BankAccount **head){
+void readAccountInfo(const char *file_name, BankAccount **head) {
   // we used fopen from stdio.h with parameter r to open the .txt file
   // and return a pointer if successful
   FILE *file = fopen(file_name, "r");
-  if (file == NULL){
+  if (file == NULL) {
     printf("Error Accessing file\n");
-    return;
+    return; // we exit if open isn't successful
   }
 
   char line[max_len];
-  while (fgets(line, sizeof(line), file)) {
+  while (fgets(line, sizeof(line), file) != NULL) {
     char FirstName[15];
     char LastName[15];
     int Number;
     int Code;
     float Balance;
-    if (sscanf(line, "%s %s %d %d %f", FirstName, LastName, &Number, &Code, &Balance) == 5) {
+    if (sscanf(line, "%s %s %d %d %f", FirstName, LastName, &Number, &Code,
+               &Balance) == 5) {
+      // here in the condition we checked if all the data is in the line using
+      // sscanf it returns an int of how many variable it had read(basically
+      // like scanf)
       addAccountToList(head, FirstName, LastName, Number, Code, Balance);
     } else {
       printf("You Have Entered Wrong The wrong type\n");
@@ -50,10 +55,15 @@ void readAccountInfo(const char *file_name, BankAccount **head){
   fclose(file);
 }
 
-void writeAccountInfo(const char *file_name){
-  FILE *file = fopen(file_name, "r");
-  if (file == NULL){
+void writeAccountInfo(const char *file_name, BankAccount *AddedAcc) {
+  FILE *file = fopen(file_name, "a"); // here we used append mode
+  // Instead of write mode which doesn't overwirte the file
+  if (file == NULL) {
     printf("Error Accessing file\n");
     return;
   }
+  const BankAccount *temp;
+  fprintf(file, "%s %s %d %d %f", AddedAcc->Name.First, AddedAcc->Name.Last,
+          AddedAcc->Number, AddedAcc->Code, AddedAcc->Balance);
+  fclose(file);
 }
